0982e205d6603248357c4d8374718914308e8f08
高杉宗汰 <sotatk25@KetsuzeiMacBook-Air.local>
2023-12-17 23:20:41 +0900

    USER機能実装完了

---
+++ src/main/java/org/springframework/samples/petclinic/service/UserService.java
@@ -0,0 +1,9 @@
+package org.springframework.samples.petclinic.service;
+
+import org.springframework.samples.petclinic.model.User;
+
+public interface UserService {
+
+	void saveUser(User user);
+
+}
